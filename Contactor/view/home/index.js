c125f114769256019ff29b6c15a6d39fa95e95bc
brekigunnars <147734485+brekigunnars@users.noreply.github.com>
2024-12-02 12:34:16 +0000

    Setup

---
+++ Contactor/view/home/index.js
@@ -0,0 +1,6 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, View} from 'react-native';
+import * as FileSystem from 'expo-file-system';
+import { shareAsync } from 'expo-sharing';
+
+expo
